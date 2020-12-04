730bf6d86c1eedabe954fe09ff0c659e47e894e6
LiHang <18625627345@163.com>
2020-12-04 23:35:36 +0800

    add des

--- labcodes/lab1/boot/bootmain.c
+++ labcodes/lab1/boot/bootmain.c
@@ -46,18 +46,18 @@ readsect(void *dst, uint32_t secno) {
     // wait for disk to be ready
     waitdisk();
 
-    outb(0x1F2, 1);                         // count = 1
-    outb(0x1F3, secno & 0xFF);
-    outb(0x1F4, (secno >> 8) & 0xFF);
-    outb(0x1F5, (secno >> 16) & 0xFF);
-    outb(0x1F6, ((secno >> 24) & 0xF) | 0xE0);
-    outb(0x1F7, 0x20);                      // cmd 0x20 - read sectors
+    outb(0x1F2, 1);                         // count = 1，0x1F2位置代表要读取扇区的数量，这里1代表只读取一个扇区
+    outb(0x1F3, secno & 0xFF);              //LBA 0~7 位 低八位         LBA28位就是要读的扇区号
+    outb(0x1F4, (secno >> 8) & 0xFF);       //LBA 8～15 位 中八位
+    outb(0x1F5, (secno >> 16) & 0xFF);      //LBA 16～23 位 高八位
+    outb(0x1F6, ((secno >> 24) & 0xF) | 0xE0);//如果是LBA模式 前4位就是LBA 24～27位，第4位为0主盘，为1从盘
+    outb(0x1F7, 0x20);                      //既是状态也是命令寄存器，第七位为1表示忙，第三位为1表示准备好了    写入0x20读取数据 写入0x30写入数据 
 
     // wait for disk to be ready
     waitdisk();
 
     // read a sector
-    insl(0x1F0, dst, SECTSIZE / 4);
+    insl(0x1F0, dst, SECTSIZE / 4);         //从0x1F0读数据
 }
 
 /* *
@@ -66,7 +66,7 @@ readsect(void *dst, uint32_t secno) {
  * */
 static void
 readseg(uintptr_t va, uint32_t count, uint32_t offset) {
-    uintptr_t end_va = va + count;
+    uintptr_t end_va = va + count;//elf程序段的起始虚拟地址加上程序段的长度，计算得出该程序段的虚拟地址的结束地址
 
     // round down to sector boundary
     va -= offset % SECTSIZE;
@@ -98,13 +98,13 @@ bootmain(void) {
     // load each program segment (ignores ph flags)
     ph = (struct proghdr *)((uintptr_t)ELFHDR + ELFHDR->e_phoff);
     eph = ph + ELFHDR->e_phnum;
-    for (; ph < eph; ph ++) {
-        readseg(ph->p_va & 0xFFFFFF, ph->p_memsz, ph->p_offset);
+    for (; ph < eph; ph ++) {                           //循环读取每个program header（注：ph是proghdr结构指针，+1即代表讲指针指向下一个proghdr结构）
+        readseg(ph->p_va & 0xFFFFFF, ph->p_memsz, ph->p_offset);//由于用的是简易的GDT，故真实地址即虚拟地址，所以第一个参数直接使用真实地址，第二个参数是段的大小，第三个参数是相对于头的偏移地址，因为内核是直接从1扇区开始写入的，readseg也是从1扇区写入的，所以不需要再添加头的基址（0）
     }
 
     // call the entry point from the ELF header
     // note: does not return
-    ((void (*)(void))(ELFHDR->e_entry & 0xFFFFFF))();
+    ((void (*)(void))(ELFHDR->e_entry & 0xFFFFFF))();   //跳转到ELFHDR的程序入口虚拟地址
 
 bad:
     outw(0x8A00, 0x8A00);
